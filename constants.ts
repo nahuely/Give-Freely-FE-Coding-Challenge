2af09aaa3555d5295c1fa49020398e2a3df1cc9e
nahuely <nahuelfamendola@hotmail.com>
2023-08-24 21:19:51 -0300

    update in constants

--- constants.ts
+++ constants.ts
@@ -1,2 +1,3 @@
-export const API_URL = process.env.PLASMO_PUBLIC_API_URL
-export const API_TOKEN = process.env.PLASMO_PUBLIC_API_TOKEN
+export const API_URL = "https://api.jsonbin.io/v3/b/64678cf09d312622a36121b8"
+export const API_TOKEN =
+  "$2b$10$QhrtefF/jKDbKgauF5trL.SK6VAk69VSIcHMhGaEs8ZViK.xBh0Om"
